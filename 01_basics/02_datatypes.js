0af2d83410b710336e582ff8073c856ae928c153
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-02 18:57:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use state"; // treat all js code as newer verion
+let name = "kaushal"//string
+let age = 8 // int
+let isLoggedIn = false // boolean
+
+
+// number => 2poweer 53
+// bigint
+// string
+// boolean
+// null => standalone 
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof"kausha")
