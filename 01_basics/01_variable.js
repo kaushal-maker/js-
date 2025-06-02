7b283acd1989c96f62e311f380f43c225b0c1d5e
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-02 18:57:00 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "'kaushal@35google.com"
+var accountPassword = "1234"
+accountCity =  "delhi"
+
+// accountId = 80864 //not allowed to chabnge const
+// accountEmail = "kaushaka@google.com" // allowed
+// accountCity = "goa"// allowed
+// accountPassword = "13627428"// allowed
+
+/* please only use let not var */
+console.log(accountEmail);
+
+console.table([accountCity, accountPassword])
