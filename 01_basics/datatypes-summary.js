68f494f9fdae6b0d018ee53bf403c656fdee29f0
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-06 16:17:45 +0000

    datatpes Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,45 @@
+// primitive(call by value)
+
+// 7 categories : String, Number , Boolean , null , undefined , symbol(for makeing any value unique) , BigInt
+
+// Js is 
+
+
+const score = 100
+const scoreValue = 100.9
+const isLoggedIn = false
+const ousideTemp = null
+let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+
+
+const bigNumber = 23456789876543098765434567n
+// Reference (Non- primitive) 
+
+// Array , Objects, Functions
+
+const heros = ['shaktiman' , 'naagraj' , 'doga']
+let myObj = {
+    name:'kaushal',
+    age:22,
+
+}
+
+
+
+
+
+// function
+
+
+const myFunction = function()
+{
+    console.log('hello word')
+}
+
+console.log(typeof bigNumber);
