999ca82e0d8c291473cecb568a57f491e06284a5
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-07 09:13:24 +0000

    string

---
+++ 01_basics/05_,string.js
@@ -0,0 +1,34 @@
+// // String
+
+// const name = 'kaushal'
+// const repoCount = 2
+
+// //console.log(name + repoCount + "Yo"); {old one for concat}
+
+// console.log(`hello my name is ${name} and ${repoCount}`);
+
+// const gameName = new String('kaushal-kp')
+
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('u'));
+
+// const newString =gameName.substring(0, 4) 
+// console.log(newString);
+
+// const anotherString =gameName.slice(-9, 5 )
+// console.log(anotherString);
+
+
+const newStringOne = " kaushal "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url ="http://kaushal.com/kaushal%20kp"
+
+console.log(url.replace('kaushal', 'k'));
+console.log(url.includes('kaushal'));
+
+console.log(url.split('_'));
+
