bd101d82a7cdc20e632d539fe92652c338e4f730
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-07 17:34:03 +0000

    number andd maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+// const score = 799
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(10));
+
+// const otherNumber = 23.8683
+
+// console.log(otherNumber.toPrecision(3));
+
+
+// const hundreads = 1000000
+// console.log(hundreads.toLocaleString('en-IN'));
+
+
+//++++++++++++++MATHS+++++++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.28));
+// console.log(Math.ceil(3.28));//Gives Uppervalue
+// console.log(Math.floor(3.28));// gives lower value
+// console.log(Math.min(4 ,4 ,76 ,56546 ,46642));
+
+console.log(Math.random);
+console.log((Math.random()*10)+1);
+
+
+const min = 10;
+const max = 20;
+const result = Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(result); // ✅ 18
+
+
