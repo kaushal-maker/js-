a4b8cbca744f33942fbf8f5a6ceace59cfdb7190
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-08 08:26:42 +0000

    Date and Time

---
+++ 01_basics/07_Date_and_Time.js
@@ -0,0 +1,25 @@
+// let myDate = new Date()
+// console.log(myDate.toString());//Sun Jun 08 2025 08:08:17 GMT+0000
+// console.log(myDate.toLocaleString());//6/8/2025, 8:08:17 AM
+// console.log(myDate.toDateString());//Sun Jun 08 2025
+// console.log(myDate.toLocaleDateString());//6/8/2025
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date("2025-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+
+let newDate = new Date()
+// console.log(newDate.getFullYear());
+
+newDate.toLocaleString('default' , {
+    weekday: "long"
+}) 
+
+
