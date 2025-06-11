5e198c08c308a60db49877a938398f6ce37fd953
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-11 11:06:27 +0000

    switch

---
+++ 04_Control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch condition
+
+// switch (key for what we are checking for ) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+
+    case "feb":
+        console.log("feburary");
+        break;
+
+    case "march":
+        console.log("march");
+        break;
+
+    default:
+        break;
+}
