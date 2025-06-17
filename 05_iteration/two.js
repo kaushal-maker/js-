32df302c6066aba381fd4dc2605862aaa02faea9
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-17 17:10:26 +0000

    while and do while

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -1,20 +1,19 @@
-
-// let index = 0
-// while (index <= 10) {
-//     console.log(`values of index is ${index}`);
-//     index = index + 2
-    
-// }
-
-let myArray = ['kaushal', 'monty', 'shaunty']
-
-let arr = 0
-while (arr < myArray.length) {
-    console.log(`value is ${myArray[arr]}`);
-    arr++;
-    
+let index = 0
+while (index <= 10) {
+    console.log(`value of index ix ${index}`);
+    index = index + 2
 }
 
+
+
+myArray = ['kaushal' ,'prince' ,'chuchundar']
+let arr = 0
+while(arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1    
+}
+
+// do while
 
 let score = 1
 do {
