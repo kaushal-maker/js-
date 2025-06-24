f5a0afb9a737f12976994d13905e5d3871144193
kaushal-maker <kaushalkpathak77@gmail.com>
2025-06-24 18:11:02 +0000

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,26 @@
+// const myObject = {
+//     py : 'python',
+//     js : 'javascript',
+//     rb: 'ruby'
+// }
+
+// for (const key in myObject) {
+//    console.log(key);//py,js , rb
+//     console.log(myObject[key]);
+//     /*py
+// python
+// js
+// javascript
+// rb
+// ruby*/
+//         console.log(`${key} for ${myObject[key]}`);
+// }
+
+
+// const programming = ['js','python']
+
+// for(const key in programming){
+//     console.log(programming);
+    
+// }
+
